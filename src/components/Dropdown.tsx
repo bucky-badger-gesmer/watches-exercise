bc18ae4ab3bdc3215804c1589c702963e7f5c662
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2024-01-04 22:55:26 -0600

    feat: add dropdown component

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,57 @@
+import { ConfigProvider, Select, theme } from "antd";
+import { useState } from "react";
+import "./Dropdown.css";
+
+const Dropdown: React.FC = () => {
+  const [selectedValues, setSelectedValues] = useState<string[]>([]);
+  const [count, setCount] = useState<number>(0);
+
+  const handleSelectChange = (value: string[]) => {
+    console.log("value", value);
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <ConfigProvider
+        theme={{
+          algorithm: theme.darkAlgorithm,
+        }}
+      >
+        <Select
+          style={{ width: "25%" }}
+          defaultValue={["Select All"]}
+          placeholder="Show All"
+          onChange={handleSelectChange}
+          options={[
+            {
+              label: "Select All",
+              value: "Select All",
+            },
+            {
+              label: "Gainers",
+              value: "Gainers",
+            },
+            {
+              label: "Losers",
+              value: "Losers",
+            },
+            {
+              label: "Hot",
+              value: "Host",
+            },
+            {
+              label: "Our Picks",
+              value: "Our Picks",
+            },
+          ]}
+        />
+      </ConfigProvider>
+
+      {count > 0 && <div className="dropdownCount">+{count}</div>}
+      {count >= 9 && <div className="dropdownCount">{count}+</div>}
+    </>
+  );
+};
+
+export default Dropdown;
