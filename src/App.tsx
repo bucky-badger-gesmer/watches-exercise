1dcb784d9bdad0e9b11af47f0f8a8fbbe5016079
bucky-badger-gesmer <aaron.gesmer@gmail.com>
2024-01-05 22:00:39 -0600

    chore: revert home delete

--- src/App.tsx
+++ src/App.tsx
@@ -28,11 +28,11 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/">
+        <Route exact path="/home">
           <Home />
         </Route>
         <Route exact path="/">
-          <Redirect to="/" />
+          <Redirect to="/home" />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
